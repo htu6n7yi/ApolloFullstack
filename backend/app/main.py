643e56cb9c3c1a291cfd064e38c3e5f9d0af9ea4
htu6n7yi <jcavalcanti008@gmail.com>
2026-01-08 19:44:46 -0300

    fix: rota inicial da API bem sucedida

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
+# backend/app/main.py
 from fastapi import FastAPI
+from . import models
+from .database import engine
+
+# Isso cria as tabelas no banco de dados automaticamente
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get('/')
+@app.get("/")
 def read_root():
-    return {'message': 'API is running'}
+    return {"message": "API do Apollo Fullstack rodando!"}
